133c43679387ec04c8d15d23b2c00edf9b273903
Abdoune <mohamed.abdoune@mobiledev-pro.fr>
2021-09-09 14:44:55 +0200

    add excel creator

---
+++ src/AbstractExcelCreator.php
@@ -0,0 +1,64 @@
+<?php
+
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Exception;
+
+
+/**
+ * Class AbstractExcelCreator
+ */
+class AbstractExcelCreator
+{
+    /**
+     * @var Spreadsheet
+     */
+    protected $spreadsheet;
+
+    /**
+     * ExcelGenerator constructor.
+     */
+    public function __construct()
+    {
+        $this->spreadsheet = new Spreadsheet();
+    }
+
+    /**
+     * @param $title
+     * @param null $description
+     */
+    protected function setHeaders($title, $description = NULL)
+    {
+        if ($description === NULL) {
+            $description = "$title document for Office 2005 XLSX !";
+        }
+
+        $this->spreadsheet->getProperties()->setCreator("GIFI-LOC")
+            ->setLastModifiedBy("PHP project - PW Starter - kit")
+            ->setTitle($title)
+            ->setDescription($description);
+    }
+
+
+    /**
+     * @param string $filename
+     * @throws Exception
+     */
+    protected function save($filename = 'upload/test.xls')
+    {
+        $writer = IOFactory::createWriter($this->spreadsheet, 'Xls');
+
+        $writer->save($filename);
+    }
+
+    /**
+     * @throws Exception
+     */
+    protected function stream()
+    {
+        $writer = IOFactory::createWriter($this->spreadsheet, 'Xls');
+        header('Content-Disposition: attachment; filename="'. urlencode('gifi-loc-users.xls').'"');
+        return $writer->save('php://output');
+    }
+
+}
